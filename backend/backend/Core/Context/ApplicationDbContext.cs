2519feac7563f26832bf243ff9ad2796fd021727
returnaction <obergannv@gmail.com>
2023-11-30 03:51:00 -0500

    Add context, install packages, made migrations

---
+++ backend/backend/Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Core.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Job>()
+                .HasOne(job => job.Company)
+                .WithMany(company => company.Jobs)
+                .HasForeignKey(job => job.CompanyId);
+
+            modelBuilder.Entity<Candidate>()
+                .HasOne(candidate => candidate.Job)
+                .WithMany(job => job.Candidates)
+                .HasForeignKey(candidate => candidate.JobId);
+                
+        }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,15 @@
+using backend.Core.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Db Configurations
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
